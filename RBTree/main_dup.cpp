78b727155548d823f493a3bf5fc7f7ed27fc9b93
dlazarosps <dlspsilva@inf.ufrgs.br>
2018-06-29 11:48:20 -0300

    [FIX] GDB compare file param

--- RBTree/main_dup.cpp
+++ RBTree/main_dup.cpp
@@ -62,36 +62,36 @@ void compare_output(char *input1, char *input2, char *detectLog) {
 
 int main(int argc, char * argv[]) {
 
-    if (argc != 4){
-        cerr << "Uso correto: ./main [input file] [output file] [log file]" << endl;
+    if (argc != 5){
+        cerr << "Uso correto: ./main [input file] [output file] [log file] [output2 file]" << endl;
         return 0;
     }
 
     RBTree rbTree1, rbTree2; //arvore R&B
-    char output2[9] = "output2_";
+    // char output2[9] = "output2_";
 
     //aplica algoritmo nas duas cópias
-    #pragma omp parallel
-    #pragma omp single
+    // #pragma omp parallel
+    // #pragma omp single
     rbTree1 = runner(argv[1]);
     rbTree2 = runner(argv[1]);
 
     //salva print da saida em arquivo output
-    #pragma omp task
+    // #pragma omp task
     freopen(argv[2],"w",stdout);
     rbTree1.inorder();
     fclose (stdout);
 
 
     // salva print da saida em arquivo output2
-    #pragma omp task
-    freopen(output2,"w",stdout);
+    // #pragma omp task
+    freopen(argv[4],"w",stdout);
     rbTree2.inorder();
     fclose (stdout);
-    
-    #pragma omp taskwait
 
-    compare_output(argv[2], output2, argv[3]);
+    // #pragma omp taskwait
+
+    compare_output(argv[2], argv[4], argv[3]);
 
     return 0;
 }
--- stats.sh
+++ stats.sh
@@ -1,36 +1,38 @@
 #!/bin/bash
-echo "SDCs: "
-grep -r -i SDC summary-carolfi.log |wc -l
-echo "Hangs: "
-grep -r -i HANG summary-carolfi.log |wc -l
-echo "Masked: "
-grep -r -i Masked summary-carolfi.log |wc -l
-echo "Failed: "
-grep -r -i Failed summary-carolfi.log |wc -l
-echo "Total: "
-grep -r -i -e 'Failed\|Masked\|HANG\|SDC' summary-carolfi.log |wc -l
+# echo "SDCs: "
+# grep -r -i SDC summary-carolfi.log |wc -l
+# echo "SDCs Detected: "
+# grep -r -i "SDC Detected" summary-carolfi.log |wc -l
+# echo "Hangs: "
+# grep -r -i HANG summary-carolfi.log |wc -l
+# echo "Masked: "
+# grep -r -i Masked summary-carolfi.log |wc -l
+# echo "Failed: "
+# grep -r -i Failed summary-carolfi.log |wc -l
 
-# TOTAL=$(grep -r -i -e 'Failed\|Masked\|HANG\|SDC' summary-carolfi.log |wc -l)
+TOTAL=$(grep -r -i -e 'Failed\|Masked\|HANG\|SDC\|"SDC Detected"' summary-carolfi.log |wc -l)
 
-# while [ $TOTAL -lt 10001 ]; do
-# 	clear
+while [ $TOTAL -lt 10001 ]; do
+	clear
 	
-# 	echo "SDCs: "
-# 	grep -r -i SDC summary-carolfi.log |wc -l
-# 	echo "Hangs: "
-# 	grep -r -i HANG summary-carolfi.log |wc -l
-# 	echo "Masked: "
-# 	grep -r -i Masked summary-carolfi.log |wc -l
-# 	echo "Failed: "
-# 	grep -r -i Failed summary-carolfi.log |wc -l
-# 	echo "Total: "
-# 	grep -r -i -e 'Failed\|Masked\|HANG\|SDC' summary-carolfi.log |wc -l
+	echo "SDCs: "
+	grep -r -i SDC summary-carolfi.log |wc -l
+	echo "SDCs Detected: "
+	grep -r -i "SDC Detected" summary-carolfi.log |wc -l
+	echo "Hangs: "
+	grep -r -i HANG summary-carolfi.log |wc -l
+	echo "Masked: "
+	grep -r -i Masked summary-carolfi.log |wc -l
+	echo "Failed: "
+	grep -r -i Failed summary-carolfi.log |wc -l
+	echo "Total: "
+	grep -r -i -e 'Failed\|Masked\|HANG\|SDC\|"SDC Detected"' summary-carolfi.log |wc -l
 
-# 	TOTAL=$(grep -r -i -e 'Failed\|Masked\|HANG\|SDC' summary-carolfi.log |wc -l)
+	TOTAL=$(grep -r -i -e 'Failed\|Masked\|HANG\|SDC\|"SDC Detected"' summary-carolfi.log |wc -l)
 	
-# 	sleep 0.2
-# done
+	sleep 0.2
+done
 
-# killall -9 watchdog.py
-# killall -9 fault_injector.py
-# killall -9 gdb
+killall -9 watchdog.py
+killall -9 fault_injector.py
+killall -9 gdb
