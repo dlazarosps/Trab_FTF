e12f26408f05bc266d2a1ca0b3b19d66faa0ae7f
dlazarosps <dlspsilva@inf.ufrgs.br>
2018-06-28 22:29:14 -0300

    [ADD] stats.sh with count watchdog (carol-fi)

---
+++ stats.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+echo "SDCs: "
+grep -r -i SDC summary-carolfi.log |wc -l
+echo "Hangs: "
+grep -r -i HANG summary-carolfi.log |wc -l
+echo "Masked: "
+grep -r -i Masked summary-carolfi.log |wc -l
+echo "Failed: "
+grep -r -i Failed summary-carolfi.log |wc -l
+echo "Total: "
+grep -r -i -e 'Failed\|Masked\|HANG\|SDC' summary-carolfi.log |wc -l
+
+# TOTAL=$(grep -r -i -e 'Failed\|Masked\|HANG\|SDC' summary-carolfi.log |wc -l)
+
+# while [ $TOTAL -lt 10001 ]; do
+# 	clear
+	
+# 	echo "SDCs: "
+# 	grep -r -i SDC summary-carolfi.log |wc -l
+# 	echo "Hangs: "
+# 	grep -r -i HANG summary-carolfi.log |wc -l
+# 	echo "Masked: "
+# 	grep -r -i Masked summary-carolfi.log |wc -l
+# 	echo "Failed: "
+# 	grep -r -i Failed summary-carolfi.log |wc -l
+# 	echo "Total: "
+# 	grep -r -i -e 'Failed\|Masked\|HANG\|SDC' summary-carolfi.log |wc -l
+
+# 	TOTAL=$(grep -r -i -e 'Failed\|Masked\|HANG\|SDC' summary-carolfi.log |wc -l)
+	
+# 	sleep 0.2
+# done
+
+# killall -9 watchdog.py
+# killall -9 fault_injector.py
+# killall -9 gdb
